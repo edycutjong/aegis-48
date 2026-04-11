89563f4cf6fc8f01e9b7c897c487e629f6afd695
Edy Cu <edy.cu@live.com>
2026-04-11 14:40:31 +0700

    chore: fix eslint flat config ignores and disable next img rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 
 export default defineConfig([
+  {
+    ignores: ['**/.next/**', '**/out/**', '**/build/**', '**/next-env.d.ts', 'coverage/**'],
+  },
   ...nextVitals,
   {
-    ignores: ['**/.next/**', '**/out/**', '**/build/**', '**/next-env.d.ts', '**/coverage/**'],
+    rules: {
+      '@next/next/no-img-element': 'off',
+    },
   },
 ]);
